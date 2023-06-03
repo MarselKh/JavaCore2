48bb1401fdbc677e8e3ec5d8acf989ca8767f3c1
Marsel <khasangan@gmail.com>
2023-06-03 17:37:02 +0500

    lesson4

--- MavenStudy/src/main/java/Triangle.java
+++ MavenStudy/src/main/java/Triangle.java
@@ -4,10 +4,13 @@ public class Triangle {
         float p = (float) ((a + b + c) * 0.5);
         float S = (float) Math.sqrt(p * (p - a) * (p - b) * (p - c));
 
-            if (a < 0 || b < 0 || c < 0 || (a + b <= c)
-                    || a + c <= b || b + c <= a) throw new myExeptionTriangle ("\"Не верные значения треугольника\"");
+        if (a < 0 || b < 0 || c < 0 ) throw new myExeptionTriangle ("\"Не верные значения треугольника:\n" +
+                "Треуго́льник — геометрическая фигура, образованная тремя отрезками, которые соединяют три точки, не лежащие на одной прямой");
 
-                System.out.println("Площадь треугольника по трём сторонам равна: " + S);
+        if ((a + b <= c) || a + c <= b || b + c <= a) throw new myExeptionTriangle ("\"Не верные значения треугольника: Неравенство треугольника\n" +
+                "Каждая сторона треугольника меньше суммы двух других сторон.\"");
+
+        System.out.println("Площадь треугольника по трём сторонам равна: " + S);
 //                return (int) Math.sqrt(p * (p - a) * (p - b) * (p - c));
 
         return (int) S;
--- MavenStudy/src/test/java/TriangleTest.java
+++ MavenStudy/src/test/java/TriangleTest.java
@@ -2,7 +2,7 @@ import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
-import org.junit.jupiter.params.provider.ValueSource;
+import org.junit.jupiter.params.provider.CsvSource;
 
 public class TriangleTest {
 
@@ -13,13 +13,21 @@ public class TriangleTest {
         Assertions.assertNotNull(result,"Good");
     }
 
-
     @ParameterizedTest
-    @ValueSource(ints = {3, 4, 5})
+    @CsvSource({ "3,4,5","0,4,5","-3,4,5"})
     @DisplayName("Функция площади треугольника по трём сторонам")
-    void functionTest2 (int a, int b, int c) throws myExeptionTriangle {
-        int result = new Triangle().function(a, b, c);
-        Assertions.assertNotNull(result,"olll");
+    void functionTest4(int a, int b, int c) throws myExeptionTriangle {
+        Triangle triangle = new Triangle();
+        int result = triangle.function(a,b,c);
+        Assertions.assertEquals(result,triangle.function (a, b, c));
     }
+
+    //    @ParameterizedTest
+//    @ValueSource(ints = {3, 4, 5})
+//    @DisplayName("Функция площади треугольника по трём сторонам")
+//    void functionTest2 (int a, int b, int c) throws myExeptionTriangle {
+//        int result = new Triangle().function(a, b, c);
+//        Assertions.assertNotNull(result,"olll");
+//    }
 
 }
